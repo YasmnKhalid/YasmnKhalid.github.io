0750f3c7dbba48746a087308deac11f51926fd26
YasmnKhalid <khalidyasmin821@gmail.com>
2026-07-27 22:07:27 +0800

    build: export production static site and update TypeScript configuration reference

---
+++ out/_next/static/chunks/app/freelance/book-a-call/page-4841733890391169.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[851,301,781,404,655,632,85,952,816,901,534,205,141,974,255,859,286,56,890],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
